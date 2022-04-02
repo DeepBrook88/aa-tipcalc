df0f683308365c9bca8226e41911bdfec58d5ffd
Christian Syväluoma <christian.syvaluoma@ha.ax>
2022-04-02 11:26:50 +0300

    switched to material design

--- app/src/main/java/ax/ha/it/aa/tipcalculator/MainActivity.kt
+++ app/src/main/java/ax/ha/it/aa/tipcalculator/MainActivity.kt
@@ -3,7 +3,9 @@ package ax.ha.it.aa.tipcalculator
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import ax.ha.it.aa.tipcalculator.databinding.ActivityMainBinding
 
@@ -14,16 +16,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupUI(binding.root.rootView)
+        supportActionBar?.hide()
         binding.tipcalcCalculate.setOnClickListener {
-            if (binding.editTipInput.text.toString().isNotBlank()
-                && binding.editPercentInput.text.toString().isNotBlank()) {
+            if (binding.editTipInput.editText?.text.toString().isNotBlank()
+                && binding.editPercentInput.editText?.text.toString().isNotBlank()) {
                 run {
-                    binding.textTipCalcResult.text = String.format("Total: %s",(binding.editTipInput.text.toString().toDouble() *
-                            (binding.editPercentInput.text.toString().toDouble() / 100 + 1)))
+                    binding.textTipCalcResult.text = String.format("Total %s",
+                        Math.round(
+                            (binding.editTipInput.editText?.text.toString().toDouble() *
+                                    (binding.editPercentInput.editText?.text.toString().toDouble() / 100 + 1)
+                            ) * 100
+                        ) / 100.0
+                    )
                     binding.editTipInput.clearFocus()
                     binding.editPercentInput.clearFocus()
                     hideKeyboard(this, binding.root.rootView)
                 }
+            }
+        }
+        binding.editTipInput.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus) {
+                hideKeyboard(this, binding.root.rootView)
+            }
+        }
+    }
+    private fun setupUI(view: View) {
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            view.setOnTouchListener { v, event ->
+                // v.performClick()
+                hideKeyboard(this, v)
+                v.clearFocus()
+                false
+            }
+        }
+        //If a layout container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val innerView = view.getChildAt(i)
+                setupUI(innerView)
             }
         }
     }
@@ -32,4 +64,5 @@ class MainActivity : AppCompatActivity() {
             context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0)
     }
+
 }
